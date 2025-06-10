be911831a2291ef3e248e8b6ea9d9d919cbbcf14
enmanuelmorego <143163660+enmanuelmorego@users.noreply.github.com>
2025-06-10 22:42:14 +0100

    implementation of fk_level, draft1

--- scratch.py
+++ scratch.py
@@ -1,36 +1,21 @@
 import PartOne as po
+import nltk
 
 
 df = po.read_novels()
 text2 = df.iloc[0,0]
+print(df.iloc[0,1])
 
 
 # text = "This is the test string. ThIS contains NUMBERS like 123,123,4321. Also puncutation like €#,! Is numbERS"
+cmudict = nltk.corpus.cmudict.dict()
+t2 = po.get_fks(df)
 
-w = 'beautiful'
-v = ['a','e','i','o','u']
-vowel_cons_count = 0
-syl_count = 0
+text = "The artist is the creator of beautiful things.  To reveal art and conceal the artist is art's aim.  The critic is he who can translate into another manner or a new material his impression of beautiful things."
 
-# for l in w:
-#     if l in v:
-#         vowel_cons_count += 1
-#     else:
-#         if vowel_cons_count > 0:
-#             syl_count += 1
-#             vowel_cons_count = 0
-#         else:
-#             vowel_cons_count = 0
+t = po.fk_level(text, cmudict)
 
-# print(f"my count{syl_count}")
-test_text = """Dr. Lee arrived at 3 p.m.\nHe said, "Let's begin the test — quickly!"\tWhy? Because time's short...\vAnyway, the robot (Model X-2025) passed all 3 phases. Incredible, right? Let's do it again."""
 
-t = po.get_ttrs(df)
 print("="*80)
-for key, value in t.items():
-    print(f"{key:<50} {value}")
-# t = "Wow! In 2025, Amelia's\n\n robot baked 32 delicious apple pies — can you believe it?"
-# print(nltk.tokenize.sent_tokenize(t))
-
-# test_text = "Does it Remove \n\n these types of \t\v special characters?"
-# print(po.tokens_clean(test_text))
+print(f"Dorian Grey Extract: Readbility raw {t}") 
+print(t2)
