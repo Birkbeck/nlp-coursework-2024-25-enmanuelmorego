f4580c27095fb472cd8f5ada68b97056b133892b
enmanuelmorego <143163660+enmanuelmorego@users.noreply.github.com>
2025-06-10 06:07:13 +0100

    added test file to ensure functions work as expected even after changes

--- scratch.py
+++ scratch.py
@@ -4,14 +4,15 @@ df = po.read_novels()
 text2 = df.iloc[0,0]
 
 
-text = "Hello there! In 2023, NASA launched 4.5 rockets — that's 50% more than last year. Is this the future? Maybe. iPhone sales soared, tHe world watched."
+text = "This is the test string. ThIS contains NUMBERS like 123,123,4321. Also puncutation like €#,! Is numbERS"
+
 
 
 print(po.nltk_ttr(text))
 
-for index, row in df.iterrows():
-    print(f"Row {index:<2}: {row['title']:<30} ({row['year']}) Type-Token Ratio {po.nltk_ttr(row['text']):.4f}")
-print(":"*80)
-t = po.get_ttrs(df)
-for key, value in t.items():
-    print(f"{key:<10}: {value:<25}")
+# for index, row in df.iterrows():
+#     print(f"Row {index:<2}: {row['title']:<30} ({row['year']}) Type-Token Ratio {po.nltk_ttr(row['text']):.4f}")
+# print(":"*80)
+# t = po.get_ttrs(df)
+# for key, value in t.items():
+#     print(f"{key:<10}: {value:<25}")
--- test_p1.py
+++ test_p1.py
@@ -1,3 +1,15 @@
 '''
 Script to test that the functions works as expected
 '''
+import PartOne as po
+
+def test_nltk_ttr1():
+    '''
+    Test that the function recieves a text object with:
+        Letters, number and punctuation marks
+        Upper and lower case
+    Removes non-letter items, transforms all to lower case and calculates the ttr of the cleaned tokens
+    '''
+    test_text = "This is the test string. ThIS contains NUMBERS like 123,123,4321. Also puncutation like €#,! Is numbERS"
+    # Text above contains 14 tokens and  10 unique tokens (removing number and puncptuation and ignoring case. Expect TTR = 0.7143)
+    assert po.nltk_ttr(test_text) == 0.7143
