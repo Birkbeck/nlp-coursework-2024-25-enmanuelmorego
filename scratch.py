31a64670cbb504928b1313ad4fff6d98c27a4ff8
enmanuelmorego <143163660+enmanuelmorego@users.noreply.github.com>
2025-06-12 22:00:52 +0100

    testing function adjective_count

--- scratch.py
+++ scratch.py
@@ -10,7 +10,23 @@ import contractions as c
 import pickle
 import PartOne as po
 
-df = pd.read_pickle(Path.cwd() / "pickles" /"parsed.pickle")
+import spacy
 
-for doc in df['parsed']:
-    print(doc)
+nlp = spacy.load("en_core_web_sm")
+
+# Create fake data to test function
+data = {'title': ['novel1', 'novel2', 'novel3'],
+        'text': ['there was a pink happy cat',
+                 'the broccoli was soft green and hot',
+                 'the athlete went running']}
+
+df = pd.DataFrame(data)
+# Add parsed column 
+parsed_list = []
+for index, row in df.iterrows():
+    parsed_obj = nlp(row['text'])
+    parsed_list.append(parsed_obj)
+    
+df['parsed'] = parsed_list
+
+print(po.adjective_counts(df))
