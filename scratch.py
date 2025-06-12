5e937f3a51bd0e3ef1e9e3ca97d0c2efd55f1398
enmanuelmorego <143163660+enmanuelmorego@users.noreply.github.com>
2025-06-12 22:42:52 +0100

    developing subjects_by_verb basic

--- scratch.py
+++ scratch.py
@@ -9,6 +9,7 @@ import nltk
 import contractions as c
 import pickle
 import PartOne as po
+from collections import Counter
 
 import spacy
 
@@ -31,4 +32,16 @@ df['parsed'] = parsed_list
 
 expect = [('happy', 2), ('delicious', 1), ('jealous', 1), ('soft', 2), ('green', 1), ('hot', 3), ('tall', 1), ('fast', 1)]
 
-print(po.adjective_counts(df))
+# print(po.adjective_counts(df))
+from spacy.symbols import nsubj, VERB
+doc = "The cat was running for the fish. The fish was swimming away, the cat could not catch the fish"
+doc_p = nlp(doc)
+
+verb_subj = {}
+targets = Counter()
+for possible_subject in doc_p:
+    if possible_subject.dep == nsubj and possible_subject.head.pos == VERB:
+        head = possible_subject.head
+        #verb_subj[head, possible_subject] = verb_subj.get([head, possible_subject], 0) +1
+        targets[(head.lemma_, possible_subject.lemma_)] += 1
+print(targets)
