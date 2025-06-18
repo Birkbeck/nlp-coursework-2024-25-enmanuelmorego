da0861f2cddf200edcb4d0d3af44144a8df8490e
enmanuelmorego <143163660+enmanuelmorego@users.noreply.github.com>
2025-06-18 06:18:24 +0100

    developing function to clean speeches data

--- test_p2.py
+++ test_p2.py
@@ -1,7 +1,8 @@
+import pytest
 import PartTwo as pt
 
 
-def speeches_clean1(): 
+def test_speeches_clean1(): 
     '''
     Test that Labour (Co-op) does not exist in the data frame
     Test that Labour count = 8038
@@ -9,7 +10,11 @@ def speeches_clean1():
     '''
     # Load data and ensure it contains expected value
     df = pt.read_speeches_csv()
-    assert 'Labour (Co-op)' in df['party']
+    assert 'Labour (Co-op)' in df['party'].values
+    assert df.shape == (40000,8)
 
     df_clean = pt.speeches_clean(df)
-    assert 'Labour (Co-op)' not in df['party']
+    assert 'Labour (Co-op)' not in df_clean['party'].values
+    assert df_clean.shape == (40000,8)
+    assert df_clean[df_clean['party']=='Labour'].shape[0] == 8038
+    
