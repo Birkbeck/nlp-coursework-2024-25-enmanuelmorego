f15a9557fa37be8c00017e7c7dfa43fd42761215
enmanuelmorego <143163660+enmanuelmorego@users.noreply.github.com>
2025-06-20 16:48:20 +0100

    implementing data_pre_processing

--- PartTwo.py
+++ PartTwo.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import pandas as pd
 import os
 from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def read_speeches_csv(path=Path.cwd() / "texts" / "p2-texts"):
@@ -22,13 +23,6 @@ def read_speeches_csv(path=Path.cwd() / "texts" / "p2-texts"):
     # Read data
     df = pd.read_csv(file_load)
     return df
-
-def data_pre_processing(df):
-    '''
-    Function to pre process the data in preparation for ML models.
-        Splits into train, test sets
-        Vectorises the data
-    '''
 
 def speeches_clean(df):
     '''
@@ -59,6 +53,32 @@ def speeches_clean(df):
     df_cleaned2 = df_cleaned2[df_cleaned2['speech_class'] == 'Speech']
 
     return df_cleaned2
+
+def data_pre_processing(df):
+    '''
+    Function to pre process the data in preparation for ML models.
+        Splits into train, test sets
+        Vectorises the data
+    '''
+    # Generate object that splits data
+    splitter_obj = StratifiedShuffleSplit(n_splits = 1, test_size = 0.2, random_state = 26) 
+    # Split data
+    for train_index, test_index in splitter_obj.split(df, df['party']):
+        train = df.iloc[train_index]
+        test = df.iloc[test_index]
+
+    # Split target in both training and testing set
+    y_train, y_test = train['party'], test['party']
+
+    # Create vectorised data for x objects
+    '''Prompt indicated to use all default parameters, except for ommitting English stop words and max_featrues which needs to be set to 3000'''
+    vectorizer = TfidfVectorizer(max_features = 3000, stop_words='english')
+    x_train = vectorizer.fit_transform(train['speech'])
+    x_test = vectorizer.transform(test['speech'])
+
+    # Return objects
+    return x_train, x_test, y_train, y_test, train['speech'], test['speech']
+   
 
 if __name__ == "__main__":
     
