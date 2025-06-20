1d399b122206c742180230ddbfbcb3d83878c414
enmanuelmorego <143163660+enmanuelmorego@users.noreply.github.com>
2025-06-20 16:11:08 +0100

    had to update pipfile.lock to instal sklearn

--- PartTwo.py
+++ PartTwo.py
@@ -2,7 +2,7 @@
 from pathlib import Path
 import pandas as pd
 import os
-
+from sklearn.model_selection import StratifiedShuffleSplit
 
 
 def read_speeches_csv(path=Path.cwd() / "texts" / "p2-texts"):
@@ -23,6 +23,13 @@ def read_speeches_csv(path=Path.cwd() / "texts" / "p2-texts"):
     df = pd.read_csv(file_load)
     return df
 
+def data_pre_processing(df):
+    '''
+    Function to pre process the data in preparation for ML models.
+        Splits into train, test sets
+        Vectorises the data
+    '''
+
 def speeches_clean(df):
     '''
     Function that takes a data frame containing speeches, and performs custom cleaning tasks on it
@@ -42,12 +49,25 @@ def speeches_clean(df):
     df_cleaned = df_cleaned[df_cleaned['party'] != 'Speaker']
 
     # (a).ii Remove rows where the value of 'party' is not one of the 4 most common parties
-    parties_count = df_cleaned['party'].value_counts().sort_values()
+    parties_count = df_cleaned['party'].value_counts().sort_values(ascending=False)
     # # Extract the name of the 4 most common parties 
     top4_parties = parties_count.index[:4].tolist()
     # # Filter to top 4 most common parties
     df_cleaned2 = df_cleaned[df_cleaned['party'].isin(top4_parties)]
 
- 
+    # (a).iii Remove rows where value in 'speech_class' is not 'Speech
+    df_cleaned2 = df_cleaned2[df_cleaned2['speech_class'] == 'Speech']
 
     return df_cleaned2
+
+if __name__ == "__main__":
+    
+    '''Section A'''
+    print("\n\t\t==== Section A ====\n")
+    
+    # Load speeches data frame
+    df = read_speeches_csv()
+    # Clean data frame
+    df_cleaned = speeches_clean(df)
+    print(f"Dimensions of cleaned speeches data frame")
+    print(df_cleaned.shape)
